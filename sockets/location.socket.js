340d38b39a1d7f8af3f503297dfa10374f642352
Bikash-Dushad <bikashghll@gmail.com>
2026-01-22 22:18:08 +0530

    updated code

--- sockets/location.socket.js
+++ sockets/location.socket.js
@@ -42,9 +42,28 @@ module.exports = (io) => {
     console.log("User connected:", socket.id);
 
     // Store user location ONCE
-    socket.on("USER_LOCATION", ({ lat, lng }) => {
-      socket.userLocation = { lat, lng };
-      console.log("User location set:", lat, lng);
+    socket.on("USER_LOCATION", async ({ lat, lng }) => {
+      try {
+        const captains = await redis.georadius(
+          "captains:online",
+          lng,
+          lat,
+          5,
+          "km",
+          "WITHCOORD"
+        );
+
+        const result = captains.map(([id, [lng, lat]]) => ({
+          id,
+          lat: Number(lat),
+          lng: Number(lng),
+        }));
+
+        socket.emit("NEARBY_CAPTAINS", result);
+        } catch (err) {
+        console.error("❌ Redis fetch error:", err);
+        socket.emit("NEARBY_CAPTAINS", []);
+      }
     });
 
     socket.on("disconnect", () => {
